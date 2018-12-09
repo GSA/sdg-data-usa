473512bef0f88ae3d9d2ea7af78347d64a8362e3
Brock Fanning <brockfanning@gmail.com>
2018-12-09 12:11:18 -0500

    Deploy to subfolders of the gh-pages branch instead of in the root.

--- scripts/deploy/circleci/deploy_prod.sh
+++ scripts/deploy/circleci/deploy_prod.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
-# This script deploys the built site to the prod-pages branch of the same repo.
-git config --global user.email $GH_EMAIL
-git config --global user.name $GH_NAME
+# This script deploys the built site to a subfolder of the gh-pages branch.
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_NAME"
 
-# CircleCI will identify the SSH key with a "Host" of gh-prod. In order to tell
+# CircleCI will identify the SSH key with a "Host" of gh-stg. In order to tell
 # Git to use this key, we need to hack the SSH key:
 sed -i -e 's/Host gh-stg/Host gh-stg\n  HostName github.com/g' ~/.ssh/config
 git clone git@gh-stg:$GH_ORG_STG/$CIRCLE_PROJECT_REPONAME.git out
 
 cd out
-git checkout prod-pages || git checkout --orphan prod-pages
-git rm -rfq .
-cd ..
-
-# The fully built site is already available at /tmp/build.
-cp -a /tmp/build/_site/. out/.
-
-mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
-cd out
+git checkout gh-pages || git checkout --orphan gh-pages
+# Clear the "prod" subfolder and copy the built site into it.
+git rm -rfq prod
+cp -a /tmp/build/_site prod
 
 git add -A
 git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 
-git push origin prod-pages
+git push origin gh-pages
